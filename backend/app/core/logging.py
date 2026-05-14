f0b41403e2f59a86c67d6770a1642e1178fd8834
saif20-sketch <saif20@squ.edu.om>
2026-05-14 07:50:19 +0400

    Create logging.py

---
+++ backend/app/core/logging.py
@@ -0,0 +1,19 @@
+import logging
+import structlog
+
+def configure_logging(log_level: str = "INFO") -> None:
+    logging.basicConfig(level=getattr(logging, log_level.upper(), logging.INFO))
+
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.add_log_level,
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(
+            getattr(logging, log_level.upper(), logging.INFO)
+        ),
+        cache_logger_on_first_use=True,
+    )
